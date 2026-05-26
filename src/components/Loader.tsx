3e710a89d97c0acbb922f448333813c86465232c
Ronit Mexson <ronitmexson9@gmail.com>
2026-05-26 11:34:58 +0530

    removed the import react

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const LOADER_DURATION = 2400; // ms before exit starts
 const EXIT_DURATION   = 800;  // ms for slide-up exit
